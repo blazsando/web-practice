9b147c5e9571ca2073f31d8eda7423d7b022c918
lazlo <czinege.laszlo3@gmail.com>
2022-08-30 11:25:33 +0200

    add task 4th solution

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
+import categories
 
 app = Flask('web-practice')
 
@@ -8,6 +10,11 @@ app = Flask('web-practice')
 def index(user='world'):
     return render_template('home.html', user=user)
 
+
+@app.route('/shop')
+def shop_page():
+    shop_data = categories.data
+    return render_template('shop_page.html', shop_data=shop_data)
 
 def main():
     app.run(debug=True)
