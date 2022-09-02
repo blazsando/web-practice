de22293f2006dd72b9d831b1178ce7668cf4a1a4
juhaszszabolcs90 <juhaszszabolcs90@gmail.com>
2022-09-02 17:22:41 +0200

    add task 6 solution

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ def index():
 @app.route('/shop',methods=['GET','POST'])
 @app.route('/shop/<sport>',methods=['GET','POST'])
 def shop(sport=None):
+    chosen_product = None
     if request.method == 'POST':
         brand = request.form.get('choose-brand')
         chosen_product = categories.products[brand]
         print(chosen_product)
-
     brands = None
     shop_data = categories.data
     if sport:
         for category in shop_data:
             if category['name'] == sport:
                 brands = category['brands']
-    return render_template('shop.html', shop_data=shop_data, brands=brands, sport=sport)
+    return render_template('shop.html', shop_data=shop_data, brands=brands, sport=sport, chosen_product=chosen_product)
 
 
 def main():
