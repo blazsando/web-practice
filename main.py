495ce1fdae2966b4348b54bbfb7bb933c31635f9
lazlo <czinege.laszlo3@gmail.com>
2022-09-02 16:54:41 +0200

    solve first half of task 6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 import categories
 
@@ -11,16 +11,21 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/shop')
-@app.route('/shop/<sport>')
+@app.route('/shop',methods=['GET','POST'])
+@app.route('/shop/<sport>',methods=['GET','POST'])
 def shop(sport=None):
+    if request.method == 'POST':
+        brand = request.form.get('choose-brand')
+        chosen_product = categories.products[brand]
+        print(chosen_product)
+
     brands = None
     shop_data = categories.data
     if sport:
         for category in shop_data:
             if category['name'] == sport:
                 brands = category['brands']
-    return render_template('shop.html', shop_data=shop_data, brands=brands)
+    return render_template('shop.html', shop_data=shop_data, brands=brands, sport=sport)
 
 
 def main():
