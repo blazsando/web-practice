35a985482fab36f94f5f44034e8ba2e79ac3ddf8
ujvariroland <ujvari.roland20@gmail.com>
2022-09-06 14:18:14 +0200

    dying

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import categories
 
 app = Flask('web-practice')
@@ -9,11 +9,20 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/shop/<category>')
+@app.route('/shop/<category>', methods=['POST', 'GET'])
 @app.route('/shop')
 def shop(category=None):
+    products = None
+    if request.method == 'POST':
+        brand= request.form.get('brand')
+        products = categories.products[brand]
+    brands = None
     sports = categories.data
-    return render_template('shop.html', categories=sports)
+    if category:
+        for sport in sports:
+            if category == sport['category']:
+                brands = sport['brands']
+    return render_template('shop.html', categories=sports, brands=brands, products=products)
 
 
 def main():
